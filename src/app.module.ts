33bb690d78b300ae47740ca0a421ac128d59837e
kaua <kauagabrielfarias18@gmail.com>
2025-12-17 16:39:58 -0300

    fix: database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
-import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
 import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { AppController } from './app.controller';
-import { ConfigModule } from '@nestjs/config';
 import { ProdService } from './data/services/prod.service';
+
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
     TypeOrmModule.forRootAsync({
       useClass: ProdService,
       imports: [ConfigModule],
@@ -26,4 +27,4 @@ import { ProdService } from './data/services/prod.service';
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
