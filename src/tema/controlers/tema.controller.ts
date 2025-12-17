901a280a35ed3397ce903f5d40a1ec7b3e7a8498
kaua <kauagabrielfarias18@gmail.com>
2025-12-17 19:49:20 -0300

    Crud do Tema & JwTAuthGuard em usuario

--- src/tema/controlers/tema.controller.ts
+++ src/tema/controlers/tema.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put, UseGuards } from "@nestjs/common";
 import { TemaService } from "../services/tema.service";
 import { Tema } from "../entities/tema.entity";
+import { JwtAuthGuard } from "../../auth/guard/jwt-auth.guard";
 
+@UseGuards(JwtAuthGuard)
 @Controller('/temas')
 export class temaControler {
     constructor(private readonly temaService: TemaService) {}
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Tema[]> {
+        return this.temaService.findAll();
+    }
 
     @Get('/:id')
     @HttpCode(HttpStatus.OK)
@@ -13,4 +21,27 @@ export class temaControler {
 
     }
 
+    @Get('/descricao/:descricao')
+    @HttpCode(HttpStatus.OK)
+    findAllByDescricao(@Param('descricao') descricao: string): Promise<Tema[]> {
+        return this.temaService.findAllByDescricao(descricao);
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() tema: Tema): Promise<Tema> {
+        return this.temaService.create(tema);
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() tema: Tema): Promise<Tema> {
+        return this.temaService.update(tema);
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id', ParseIntPipe) id: number) {
+        return this.temaService.delete(id);
+    }
 }
--- src/usuario/controllers/usuario.controller.ts
+++ src/usuario/controllers/usuario.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put, UseGuards } from "@nestjs/common";
 import { UsuarioService } from "../services/usuario.service";
 import { Usuario } from "../entities/usuario.entity";
 import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { JwtAuthGuard } from "../../auth/guard/jwt-auth.guard";
 
 @ApiTags('Usuario')
 @ApiBearerAuth()
@@ -9,12 +10,14 @@ import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 export class UsuarioController {
     constructor(private readonly usuarioService: UsuarioService) {}
 
+    @UseGuards(JwtAuthGuard)
     @Get('/all')
     @HttpCode(HttpStatus.OK)
     findAll(): Promise<Usuario[]> {
         return this.usuarioService.findAll();
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('/:id')
     @HttpCode(HttpStatus.OK)
     findById(@Param('id', ParseIntPipe) id: number): Promise<Usuario> {
@@ -27,6 +30,7 @@ export class UsuarioController {
         return await this.usuarioService.create(usuario);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Put('/atualizar')
     @HttpCode(HttpStatus.OK)
     async update(@Body() usuario: Usuario): Promise<Usuario> {
